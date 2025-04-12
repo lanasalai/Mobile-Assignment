8f503ad61e846a9ae79facdbc9e36868e8d68754
lana.salai <lanasalai95@gmail.com>
2025-04-12 13:34:11 +0200

    Added project
    + set minimum deployment target -> iOS 13.4

---
+++ MyPortfolio/MyPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPortfolio
+//
+//  Created by Lana Salai on 12.4.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
